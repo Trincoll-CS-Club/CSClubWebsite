22fa3b5b25cd9d9f4415aa39ac412396f3416b38
Anupam Khargharia <kharghariaanupam07@gmail.com>
2024-03-25 21:33:18 -0400

    Add non-persistent navbar

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,15 +1,19 @@
 import * as React from "react";
 import PersonCard from "./Components/PersonCard";
+import NavBar from "./Components/NavBar"
 
 const App = () => {
   return (
-    <PersonCard
-      name="Anupam Khargharia"
-      position="Vice President"
-      imageSrc="https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg"
-      emailAddress="akhargha@trincoll.edu"
-      linkedinUrl="https://www.linkedin.com/in/akhargha/"
-    />
+    <div>
+      <NavBar />
+      <PersonCard
+        name="Anupam Khargharia"
+        position="Vice President"
+        imageSrc="https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg"
+        emailAddress="akhargha@trincoll.edu"
+        linkedinUrl="https://www.linkedin.com/in/akhargha/"
+      />
+    </div>
   );
 };
 
---
+++ website/src/Components/NavBar.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Typography from "@mui/joy/Typography";
+import { CssVarsProvider } from "@mui/joy/styles";
+import CssBaseline from "@mui/joy/CssBaseline";
+
+const NavBar = () => {
+  return (
+    <CssVarsProvider>
+      <CssBaseline />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "16px",
+          bgcolor: "warning.100",
+          "&:hover": {
+            bgcolor: "warning.200",
+          },
+        }}
+      >
+        <Typography level="h5" component="div">
+          Trinity College CS Club Logo
+        </Typography>
+
+        <Box sx={{ display: "flex", gap: "2em" }}>
+          <Button variant="solid" color="warning">About Us</Button>
+          <Button variant="solid" color="warning">Explore</Button>
+          <Button variant="solid" color="warning">Join Now</Button>
+        </Box>
+      </Box>
+    </CssVarsProvider>
+  );
+};
+
+export default NavBar;
