1c6f6fc185ff6d155730e90327aaeb5f5e991c43
Anupam Khargharia <kharghariaanupam07@gmail.com>
2024-04-10 15:15:30 -0400

    separate board and product_dev in about us

--- website/src/About-small.jsx
+++ website/src/About-small.jsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import PersonCard from "./Components/PersonCard";
 import NavBar from "./Components/NavBar";
-import people from "./Data/people.json";
+import board from "./Data/board.json";
+import product from "./Data/product_dev.json";
 import Grid from "@mui/joy/Grid";
 import Typography from "@mui/joy/Typography";
 import Footer from "./Components/Footer";
@@ -40,7 +41,7 @@ const About = () => {
         level="h1"
         style={{ textAlign: "center", paddingTop: "70px", fontSize: "3.5rem", color: "#ff006e" }}
       >
-        The Team
+        Board Members
       </Typography>
 
       <Grid
@@ -50,20 +51,53 @@ const About = () => {
       >
         {" "}
         {/* Add a Grid container here */}
-        {people.map((people_data, index) => (
+        {board.map((board_data, index) => (
           <Grid item xs={3} key={index}>
             {" "}
-            {/* Each FixtureBox as a Grid item */}
             <PersonCard
-              name={people_data.name}
-              position={people_data.position}
-              imageSrc={people_data.imageSrc}
-              emailAddress={people_data.emailAddress}
-              linkedinUrl={people_data.linkedinUrl}
+              name={board_data.name}
+              position={board_data.position}
+              imageSrc={board_data.imageSrc}
+              emailAddress={board_data.emailAddress}
+              linkedinUrl={board_data.linkedinUrl}
             />
           </Grid>
         ))}
       </Grid>
+
+      <Typography
+        level="h1"
+        style={{ textAlign: "center", paddingTop: "70px", fontSize: "3.5rem", color: "#ff006e" }}
+      >
+        Product Developers
+      </Typography>
+
+      <Grid
+        container
+        spacing={2}
+        sx={{ paddingLeft: "140px", paddingTop: "50px" }}
+      >
+        {" "}
+        {/* Add a Grid container here */}
+        {product.map((product_data, index) => (
+          <Grid item xs={3} key={index}>
+            {" "}
+            <PersonCard
+              name={product_data.name}
+              position={product_data.position}
+              imageSrc={product_data.imageSrc}
+              emailAddress={product_data.emailAddress}
+              linkedinUrl={product_data.linkedinUrl}
+            />
+          </Grid>
+        ))}
+      </Grid>
+
+
+
+
+
+
       <div style={{ paddingTop: "20px" }}>
         <Footer />
       </div>
