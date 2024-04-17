6fdfb3f97394aeeb3afd5db4944a331754ca4c99
Shivanshu <shivanshu.dwivedi@trincoll.edu>
2024-04-17 16:09:06 -0400

    Connected Frontend and Backend

--- website/src/Components/MembersComponent.jsx
+++ website/src/Components/MembersComponent.jsx
@@ -1,36 +1,48 @@
 import React, { useState, useEffect } from 'react';
+import client from '../sanityClient';  // Adjust the path according to where your Sanity client is configured
+import PersonCard from './PersonCard';
+import Grid from '@mui/joy/Grid';
 
 const MembersComponent = () => {
   const [members, setMembers] = useState([]);
 
-  // Define the API endpoint URLs
-  const membersUrl = 'http://localhost:1337/api/members';
-  
-  // Define your API token here
-  const apiToken = 'ee69bd8f3ad4a6ef6510d91cf465a8ec0427e6e72217547df28f152a1cf58def6fc4650cb45cd3a1e3b7d133039fc0166918d9f2ad02b4a72a811f6d985174592125114348d66cc176f9c0fe4cf76af717d99e702539bfc81d3f59e65a0a8e8e8805d278717c5737489dcb7deb625c49a6530302b49f25a079f272cbfd6d8588';
-
   useEffect(() => {
-    fetch(membersUrl, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${apiToken}`
+    // Adjust the query as per your schema definitions
+    const query = `*[_type == "aboutUs"]{
+      name,
+      position,
+      email,
+      linkedin,
+      picture {
+        asset->{
+          _id,
+          url
+        },
+        alt
       }
-    })
-    .then(response => response.json())
-    .then(data => {
-      setMembers(data.data);
-    })
-    .catch(error => console.error('Error fetching members:', error));
-  }, []); // The empty array ensures this effect runs only once after the component mounts
+    }`;
+
+    client.fetch(query)
+      .then(data => {
+        setMembers(data);
+      })
+      .catch(error => console.error('Error fetching board members data:', error));
+  }, []);
 
   return (
-    <div id="members">
+    <Grid container spacing={2} sx={{ paddingLeft: "140px", paddingTop: "50px" }}>
       {members.map((member, index) => (
-        <p key={index}>
-          Name: {member.attributes.Name}, Position: {member.attributes.Position}
-        </p>
+        <Grid item xs={3} key={index}>
+          <PersonCard
+            name={member.name}
+            position={member.position}
+            imageSrc={member.picture?.asset?.url}
+            emailAddress={member.email}
+            linkedinUrl={member.linkedin}
+          />
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 };
 
--- website/src/sanityClient.js
+++ website/src/sanityClient.js
@@ -4,8 +4,9 @@ import {createClient} from '@sanity/client'
 export const client = createClient({
     projectId: '911guljk',
     dataset: 'production',
-    useCdn: true, 
+    useCdn: false, 
     apiVersion: '2023-04-17', // We will have to change this in order for us to be able to use the latest API generattions
     
   })
 export default client;
+
